a669d7029998ceb0696fe1b0e81226ce22d04fb3
DJ <dmitri.jurovski@gmail.com>
2018-06-06 17:32:42 +0300

    Modify js

--- js/custom.js
+++ js/custom.js
@@ -1,57 +1,84 @@
+"use strict";
+
+// Tooltips
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+
 $('main').load('home.html');
 
 // Main Menu
 $('#mainMenu a').on('click', function(e){
 	
 	$('#mainMenu a').removeClass("active");
-    $('main').load(e.target.id + '.html');
+    $('main').load(e.currentTarget.id + '.html');
 	$(this).addClass('active');
+	console.log('Load part webpage');
+	$("html, body").animate({ scrollTop: 0 }, "slow");
+
+});
+$(document).ready(function(){
+$('#resumeButton').on('click', function(e){
+	
+	$('main').load('resume.html');
+	$("html, body").animate({ scrollTop: 0 }, "slow");
+});
+
+
+$('#scrollTop').on('click', function(e){
+	$("html, body").animate({ scrollTop: 0 }, "slow");
+});
 
 });
 
 
-// Instagram Fetch data
 
-// Couseresel
- 
+function instLoad(token){
 
- var token = '3970588176.1677ed0.7477ad46f0224a60b9ab308f4b209d90',
-    num_photos = 9;
- 
-$.ajax({
+	$( "#perRow div" ).remove();
+ 	$.ajax({
 	url: 'https://api.instagram.com/v1/users/self/media/recent',
 	dataType: 'jsonp',
 	type: 'GET',
-	data: {access_token: token, count: num_photos},
+	data: {access_token: token, count: 9},
 	success: function(data){
- 		console.log(data);
+ 		console.log(data.data);
 		for( x in data.data ){
-			$('#perRow').append('<div class"col-md-4"><img class"img-fluid" src="'+data.data[x].images.standard_resolution.url+'"></div>');
+			$('#perRow').append('<div class"col-md-4"><a href="'+data.data[x].link+'" target="_blanck"><img class"img-fluid" src="'+data.data[x].images.standard_resolution.url+'"></a></div>').fadeIn(4000);
 		}
 	},
 	error: function(data){
 		console.log(data);
 	}
 });
+}
 
- var token = '7562922180.1677ed0.fc4e273eea1049be86e4a335cb642dbc',
-    num_photos = 9;
- 
-$.ajax({
-	url: 'https://api.instagram.com/v1/users/self/media/recent',
-	dataType: 'jsonp',
-	type: 'GET',
-	data: {access_token: token, count: num_photos},
-	success: function(data){
- 		console.log(data);
-		for( x in data.data ){
-			$('#artRow').append('<div class"col-md-4"><img class"img-fluid" src="'+data.data[x].images.standard_resolution.url+'"></div>');
-		}
-	},
-	error: function(data){
-		console.log(data);
-	}
+
+
+$(document).ready(function(){
+    $('#personal').on('click', function(){
+
+    	const personal = '3970588176.1677ed0.7477ad46f0224a60b9ab308f4b209d90';
+        instLoad(personal);
+        console.log("Personal Instagram Photos Load");
+    });
+
+
+    $('#creation').on('click', function(){
+
+    	const creation = '7562922180.1677ed0.fc4e273eea1049be86e4a335cb642dbc';   
+        instLoad(creation);
+        console.log("Creation Instagram Photos Load");
+    });
+
 });
+
+
+
+
+
+
+
 
 
               
